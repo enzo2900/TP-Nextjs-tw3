32a14b9290446879a582241a5ab466e82c8ddc34
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2025-12-11 12:35:52 +0100

    update video page

--- apps/web/app/jeu-video/page.tsx
+++ apps/web/app/jeu-video/page.tsx
@@ -1,3 +1,19 @@
+import { findAllVideoGames } from "@workspace/database";
+import { WithId } from "mongodb";
+import { JSX, use } from "react";
+import { VideoGame } from "../../../../packages/database/src/videoGames";
+
 export default function JeuVideo() {
-  return <ul></ul>;
+    const videoGames: WithId<VideoGame>[] = use(findAllVideoGames());
+    console.log(videoGames);
+    const stringGames: JSX.Element[] = videoGames.map((game) => {
+        return (
+            <div key={game.name}>
+                <h1>{game.name}</h1>
+                <img src={game.image}></img>
+            </div>
+        );
+    });
+    const length: number = stringGames.length;
+    return <div>{length != 0 && <ul> {stringGames}</ul>}</div>;
 }
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,10 +1,10 @@
 /**
  * @workspace/database - Package de base de données MongoDB
- * 
+ *
  * Ce package fournit un client MongoDB et des modèles pour gérer les données
  * de l'application. Il est conçu pour fonctionner avec une instance MongoDB
  * locale via Docker.
- * 
+ *
  * Exports principaux :
  * - Client MongoDB : connexion et utilitaires de base de données
  * - User : modèle utilisateur pour l'authentification
@@ -12,30 +12,27 @@
 
 // Export du client MongoDB
 export {
-  getMongoClient,
-  getDatabase,
-  getCollection,
-  closeConnection,
-  checkConnection,
-} from './mongodb-client';
+    getMongoClient,
+    getDatabase,
+    getCollection,
+    closeConnection,
+    checkConnection,
+} from "./mongodb-client";
 
 // Export du modèle User et ses types
 export {
-  createUser,
-  findUserById,
-  findUserByEmail,
-  listUsers,
-  updateUser,
-  deleteUser,
-  countUsers,
-  initializeUsersCollection,
-} from './user';
+    createUser,
+    findUserById,
+    findUserByEmail,
+    listUsers,
+    updateUser,
+    deleteUser,
+    countUsers,
+    initializeUsersCollection,
+} from "./user";
 
-export type {
-  User,
-  CreateUserInput,
-  UpdateUserInput,
-} from './user';
+export { findAllVideoGames } from "./videoGames";
+export type { User, CreateUserInput, UpdateUserInput } from "./user";
 
 // Export de ObjectId pour la validation des IDs
-export { ObjectId } from 'mongodb';
+export { ObjectId } from "mongodb";
